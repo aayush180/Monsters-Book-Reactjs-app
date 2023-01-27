ad47687fd2efa629593ad5095bced38c5e57a54d
aayush180 <thakaren018@gmail.com>
2023-01-27 16:59:34 +0530

    initial commit

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+import './card.styles.css';
+
+class Card extends Component {
+  render(){
+    const {id,name}=this.props.monster;
+    return (
+        <div className='card-container' key={id}>
+        <img src={`https://robohash.org/${id}?set=set2&&size=180x180`} alt="monster" />
+        <h2>{name}</h2>
+      </div>
+    )
+  }
+
+
+}
+
+
+
+
+export default Card;
